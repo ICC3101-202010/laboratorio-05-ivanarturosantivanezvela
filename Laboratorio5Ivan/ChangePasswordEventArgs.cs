486a5f4f151461ceea30b929baf0bf0d4d075d9a
Ivan Arturo Santivañez Vela <Ivan@MacBook-Pro-de-Clementina-757.local>
2020-04-27 17:09:50 -0400

    Final

---
+++ Laboratorio5Ivan/ChangePasswordEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Laboratorio5Ivan
+{
+    public class ChangePasswordEventArgs : EventArgs
+    {
+        public string Email { get; set; }
+        public string Number { get; set; }
+        public string Username { get; set; }
+    }
+}
---
+++ Laboratorio5Ivan/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+namespace Laboratorio5Ivan
+{
+    public class User
+    {
+        public delegate void EmailVerifiedEventHandler(object source, EventArgs args);
+        public event EmailVerifiedEventHandler EmailVerified;
+
+        protected virtual void OnEmailVerified()
+        {
+            if (EmailVerified != null)
+            {
+
+                EmailVerified(this, EventArgs.Empty);
+            }
+
+
+
+        }
+
+
+        public void OnEmailSented(object source, EventArgs e)
+        {
+
+            Console.WriteLine("Desea mandar un correo de verificacion ? (Escriba su opcion numerica 1 o 2)");
+            Console.WriteLine("1) si  ");
+            Console.WriteLine("2) no  ");
+            string respuesta = Console.ReadLine();
+            if (respuesta == "1")
+            {
+                Thread.Sleep(2000);
+                OnEmailVerified();
+                
+
+            }
+            if (respuesta == "2")
+            {
+                Console.WriteLine("No se envio correo de verificacion ");
+            }
+
+
+        }
+
+
+
+    }
+}
